33b16ea7a7fd77d4fc3632a02530a921e33badc9
Milen Halachev <milenski88@gmail.com>
2017-04-11 16:50:17 +0300

    Initial Commit

---
+++ MHIdentityKit/MHIdentityKit.h
@@ -0,0 +1,19 @@
+//
+//  MHIdentityKit.h
+//  MHIdentityKit
+//
+//  Created by Milen Halachev on 4/11/17.
+//  Copyright © 2017 Milen Halachev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MHIdentityKit.
+FOUNDATION_EXPORT double MHIdentityKitVersionNumber;
+
+//! Project version string for MHIdentityKit.
+FOUNDATION_EXPORT const unsigned char MHIdentityKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MHIdentityKit/PublicHeader.h>
+
+
