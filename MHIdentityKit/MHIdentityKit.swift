5e6e39910f683a59cea0a7dad8caaa5f53d42987
Milen Halachev <milenski88@gmail.com>
2017-06-28 09:29:51 +0300

    - made the bundle identifier internal

--- MHIdentityKit/MHIdentityKit.swift
+++ MHIdentityKit/MHIdentityKit.swift
@@ -8,4 +8,4 @@
 
 import Foundation
 
-public let bundleIdentifier = Bundle(for: OAuth2IdentityManager.self).bundleIdentifier!
+let bundleIdentifier = Bundle(for: OAuth2IdentityManager.self).bundleIdentifier!
