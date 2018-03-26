de617bafd13cf3e46d4c6c6622efb57ab294293f
Milen Halachev <milenski88@gmail.com>
2018-03-26 13:21:22 +0300

    - added watchOS target - DefaultNetoworkClient - iOS specific code now compiles only on iOS

--- MHIdentityKit/Infrastructure/NetworkClient/DefaultNetoworkClient.swift
+++ MHIdentityKit/Infrastructure/NetworkClient/DefaultNetoworkClient.swift
@@ -15,24 +15,29 @@ class DefaultNetoworkClient: NetworkClient {
     
     func perform(_ request: URLRequest, completion: @escaping (NetworkResponse) -> Void) {
         
-        let application = UIApplication.shared
-        var id = UIBackgroundTaskInvalid
-        id = application.beginBackgroundTask(withName: "MHIdentityKit.DefaultNetoworkClient.\(#function).backgroundTask") {
-            
-            let description = NSLocalizedString("Unable to complete network request", comment: "The description of the network error produced when the background time has expired")
-            let reason = NSLocalizedString("Backgorund time has expired.", comment: "The reason of the network error produced when the background time has expired")
-            let error = MHIdentityKitError.general(description: description, reason: reason)
-            
-            completion(NetworkResponse(data: nil, response: nil, error: error))
-            application.endBackgroundTask(id)
-            id = UIBackgroundTaskInvalid
-        }
+        #if os(iOS)
+            let application = UIApplication.shared
+            var id = UIBackgroundTaskInvalid
+            id = application.beginBackgroundTask(withName: "MHIdentityKit.DefaultNetoworkClient.\(#function).backgroundTask") {
+                
+                let description = NSLocalizedString("Unable to complete network request", comment: "The description of the network error produced when the background time has expired")
+                let reason = NSLocalizedString("Backgorund time has expired.", comment: "The reason of the network error produced when the background time has expired")
+                let error = MHIdentityKitError.general(description: description, reason: reason)
+                
+                completion(NetworkResponse(data: nil, response: nil, error: error))
+                application.endBackgroundTask(id)
+                id = UIBackgroundTaskInvalid
+            }
+        #endif
         
         let task = self.session.dataTask(with: request) { (data, response, error) in
             
             completion(NetworkResponse(data: data, response: response, error: error))
-            application.endBackgroundTask(id)
-            id = UIBackgroundTaskInvalid
+            
+            #if os(iOS)
+                application.endBackgroundTask(id)
+                id = UIBackgroundTaskInvalid
+            #endif
         }
         
         task.resume()
