8d69032c06b3a996103285a0d3c5c4a89526b34f
Milen Halachev <milenski88@gmail.com>
2018-03-28 17:04:09 +0300

    - AuthorizationResponseType - made struct, allowing the ability to define and use custom types

--- MHIdentityKit/OAuth2/AuthorizationResponseType.swift
+++ MHIdentityKit/OAuth2/AuthorizationResponseType.swift
@@ -9,8 +9,62 @@
 import Foundation
 
 //https://tools.ietf.org/html/rfc6749#section-3.1.1
-public enum AuthorizationResponseType: String {
+public struct AuthorizationResponseType  {
     
-    case code = "code"
-    case token = "token"
+    public var value: String
+    
+    public init(value: String) {
+        
+        self.value = value
+    }
+}
+
+//predefined grant types
+extension AuthorizationResponseType {
+    
+    public static let code: AuthorizationResponseType = "code"
+    public static let token: AuthorizationResponseType = "token"
+}
+
+//MARK: - RawRepresentable
+extension AuthorizationResponseType: RawRepresentable {
+    
+    public var rawValue: String {
+        
+        get { return self.value }
+        set { self.value = newValue }
+    }
+    
+    public init?(rawValue: String) {
+        
+        self.init(value: rawValue)
+    }
+}
+
+//MARK: - ExpressibleByStringLiteral
+extension AuthorizationResponseType: ExpressibleByStringLiteral {
+    
+    public init(stringLiteral value: String) {
+        
+        self.init(value: value)
+    }
+    
+    public init(unicodeScalarLiteral value: String) {
+        
+        self.init(value: value)
+    }
+    
+    public init(extendedGraphemeClusterLiteral value: String) {
+        
+        self.init(value: value)
+    }
+}
+
+//MARK: - CustomStringConvertible
+extension AuthorizationResponseType: CustomStringConvertible {
+    
+    public var description: String {
+        
+        return self.value
+    }
 }
