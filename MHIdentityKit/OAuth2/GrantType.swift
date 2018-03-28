498266b059a945511a648bafa418aec6fd9fba39
Milen Halachev <milenski88@gmail.com>
2018-03-28 17:02:41 +0300

    - made GrantType struct, allowing the ability to define and use custom grant types

--- MHIdentityKit/OAuth2/GrantType.swift
+++ MHIdentityKit/OAuth2/GrantType.swift
@@ -8,10 +8,64 @@
 
 import Foundation
 
-public enum GrantType: String {
+public struct GrantType  {
     
-    case password = "password"
-    case refreshToken = "refresh_token"
-    case clientCredentials = "client_credentials"
-    case authorizationCode = "authorization_code"
+    public var value: String
+    
+    public init(value: String) {
+        
+        self.value = value
+    }
+}
+
+//predefined grant types
+extension GrantType {
+    
+    public static let password: GrantType = "password"
+    public static let refreshToken: GrantType = "refresh_token"
+    public static let clientCredentials: GrantType = "client_credentials"
+    public static let authorizationCode: GrantType = "authorization_code"
+}
+
+//MARK: - RawRepresentable
+extension GrantType: RawRepresentable {
+    
+    public var rawValue: String {
+        
+        get { return self.value }
+        set { self.value = newValue }
+    }
+    
+    public init?(rawValue: String) {
+        
+        self.init(value: rawValue)
+    }
+}
+
+//MARK: - ExpressibleByStringLiteral
+extension GrantType: ExpressibleByStringLiteral {
+    
+    public init(stringLiteral value: String) {
+        
+        self.init(value: value)
+    }
+    
+    public init(unicodeScalarLiteral value: String) {
+        
+        self.init(value: value)
+    }
+    
+    public init(extendedGraphemeClusterLiteral value: String) {
+        
+        self.init(value: value)
+    }
+}
+
+//MARK: - CustomStringConvertible
+extension GrantType: CustomStringConvertible {
+    
+    public var description: String {
+        
+        return self.value
+    }
 }
